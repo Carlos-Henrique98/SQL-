46e35682894afd0ac52600226a116cde2dc6c344
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-05 20:55:27 -0300

    Exercicio-012

---
+++ 012 - Taxas.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Taxas;
+
+USE Taxas;
+
+CREATE TABLE Pessoas(
+IdPessoas INT PRIMARY KEY IDENTITY,
+NomeP VARCHAR(255) NOT NULL,
+Salario DECIMAL(6,2) NOT NULL
+);
+
+INSERT INTO Pessoas VALUES ('James M. Tabarez', 883);
+INSERT INTO Pessoas VALUES ('Rafael T. Hendon', 4281);
+INSERT INTO Pessoas VALUES ('Linda J. Gardner', 4437);
+INSERT INTO Pessoas VALUES ('Nicholas J. Conn', 8011);
+INSERT INTO Pessoas VALUES ('Karol A. Morales', 2508);
+INSERT INTO Pessoas VALUES ('Lolita S. Graves', 8709);
