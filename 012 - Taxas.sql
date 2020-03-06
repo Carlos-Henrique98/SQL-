2ed4604d63c2dab1629b9125810165d9f39582ad
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-05 21:27:22 -0300

    Exercicios

--- 012 - Taxas.sql
+++ 012 - Taxas.sql
@@ -5,7 +5,8 @@ USE Taxas;
 CREATE TABLE Pessoas(
 IdPessoas INT PRIMARY KEY IDENTITY,
 NomeP VARCHAR(255) NOT NULL,
-Salario DECIMAL(6,2) NOT NULL
+Salario DECIMAL(6,2) NOT NULL,
+Taxa AS (Salario * 0.1) + Salario
 );
 
 INSERT INTO Pessoas VALUES ('James M. Tabarez', 883);
@@ -14,3 +15,10 @@ INSERT INTO Pessoas VALUES ('Linda J. Gardner', 4437);
 INSERT INTO Pessoas VALUES ('Nicholas J. Conn', 8011);
 INSERT INTO Pessoas VALUES ('Karol A. Morales', 2508);
 INSERT INTO Pessoas VALUES ('Lolita S. Graves', 8709);
+
+SELECT * FROM Pessoas;
+
+DROP TABLE Pessoas;
+
+SELECT NomeP,Taxa AS Taxa FROM Pessoas
+WHERE Salario > 3000  
