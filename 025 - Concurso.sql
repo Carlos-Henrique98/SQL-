e6769c0e83a3b48a4c1d530b478077efdbe02110
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-21 01:02:12 -0300

    Exercicio-025

--- 025 - Concurso.sql
+++ 025 - Concurso.sql
@@ -8,7 +8,7 @@ NomeC VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Ponto(
-IdPonto INT PRIMARY KEY IDENTITY,
+IdCandidato INT FOREIGN KEY REFERENCES Candidato (IdCandidato),
 Matematica INT NOT NULL,
 Especifica INT NOT NULL,
 Plano_projeto INT NOT NULL
@@ -26,19 +26,25 @@ INSERT INTO Candidato VALUES('Donna D Pursley');
 INSERT INTO Candidato VALUES('Ann C Davis');
 
 
-INSERT INTO Ponto VALUES (76, 58, 21);
-INSERT INTO Ponto VALUES (4, 5, 22);
-INSERT INTO Ponto VALUES (15, 59, 12);
-INSERT INTO Ponto VALUES (41, 42, 99);
-INSERT INTO Ponto VALUES (22, 90, 9);
-INSERT INTO Ponto VALUES (82, 77, 15);
-INSERT INTO Ponto VALUES (82, 99, 56);
-INSERT INTO Ponto VALUES (11, 4, 22);
-INSERT INTO Ponto VALUES (16, 29, 94);
-INSERT INTO Ponto VALUES (1, 7, 59);
+INSERT INTO Ponto VALUES (1, 76, 58, 21);
+INSERT INTO Ponto VALUES (2, 4, 5, 22);
+INSERT INTO Ponto VALUES (3, 15, 59, 12);
+INSERT INTO Ponto VALUES (4, 41, 42, 99);
+INSERT INTO Ponto VALUES (5, 22, 90, 9);
+INSERT INTO Ponto VALUES (6, 82, 77, 15);
+INSERT INTO Ponto VALUES (7, 82, 99, 56);
+INSERT INTO Ponto VALUES (8, 11, 4, 22);
+INSERT INTO Ponto VALUES (9, 16, 29, 94);
+INSERT INTO Ponto VALUES (10, 1, 7, 59);
+
+DROP TABLE Ponto;
+DROP TABLE Candidato;
 
 SELECT * FROM Ponto;
 SELECT * FROM Candidato;
 
-SELECT AVG((Matematica*2) + (Especifica * 3) + (Plano_Projeto * 5)) / 10 FROM Ponto;
-SELECT AVG((Matematica*2) + (Especifica * 3) + (Plano_Projeto * 5)) / 10 FROM Ponto;
+SELECT NomeC AS Candidato, ((Matematica*2) + (Especifica * 3) + (Plano_Projeto * 5)) / 10 AS Total 
+FROM Ponto P INNER JOIN Candidato C ON P.IdCandidato = C.IdCandidato
+ORDER BY Total DESC;
+
+
