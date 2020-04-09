2460a049eb4cc7f1250f1f6971e4b6ac71f62282
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-04-08 23:44:39 -0300

    Conteudo-SQL

--- TesteExemplo/Exemplo-Incremento.sql
+++ TesteExemplo/Exemplo-Incremento.sql
@@ -1,8 +1,8 @@
-DECLARE @valor INT
-SET @valor = 0
+----DECLARE @valor INT
+----SET @valor = 0
 
-WHILE @valor < 10
-   BEGIN
-       PRINT 'Número:'+CAST(@valor AS VARCHAR(2))
-       SET  @valor = @valor + 1
-   END;
+----WHILE @valor < 10
+----   BEGIN
+----       PRINT 'Número:'+CAST(@valor AS VARCHAR(2))
+----       SET  @valor = @valor + 1
+----   END;
