1e56d5a1460cedcfbd91f9021dc0e2c64d719aa2
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-05-28 17:29:11 -0300

    RedeLojas

---
+++ Exercicios/01- RedeLojas/DQL - RedeLojas - VProfessora.sql
@@ -0,0 +1,9 @@
+USE RedeLojas
+
+SELECT * FROM Armazem
+SELECT * FROM Prestador
+SELECT * FROM TipoTransacao
+SELECT * FROM Categoria
+SELECT * FROM ItemPedido
+SELECT * FROM Item
+SELECT * FROM Pedido
